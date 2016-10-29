d568819ef902ae2cc9f00cfe581817cd6fac78f1
Pablo Gamito <pablogamito@gmail.com>
2016-10-30 01:47:10 +0200

    Added to Call requests functions

--- main-contract.sol
+++ main-contract.sol
@@ -8,12 +8,14 @@ private - only this contract
 internal - only this contract and contracts deriving from it
 external - Cannot be accessed internally, only externally.*/
 
+/*TODO: Make sure user can't bet more than what he has in his balance - what is in trade because right now he can*/
+
 contract BinaryTrading is usingOraclize {
   // Used to store the contract creator's address
   address minter;
 
   // Mapping is used to make sure that requests are not processed twice
-  mapping(bytes32=>bool) myidList;
+  mapping(bytes32 => bool) myidList;
 
   // Exchange Price Variable in cents
   uint public ETHUSD;
@@ -28,11 +30,16 @@ contract BinaryTrading is usingOraclize {
   mapping (address => balance) userBalance;
   /*mapping (address => User) users;*/
 
-  struct Bet {
+  struct Bet private {
+    address userAddress;
     uint value;
-    bool putOption; // true = Put Option : false = Call Option
+    bool putOption; // true = put : false = call
+    uint openPositionPrice;
   }
-  mapping (address => Bet) bets;
+
+  mapping (uint => Bet) bets;
+  uint NewBetId;
+  mapping (bytes32 => uint) betIdWithQueryId;
 
   // This function is called upon contract creation
   function BinaryTrading() {
@@ -65,11 +72,16 @@ contract BinaryTrading is usingOraclize {
     return this.balance;
   }
 
+    // *********************** //
+   // CALL POSITION FUNCTIONS //
+  // *********************** //
+
   function internalCallRequest(address userAddress, uint betValue, uint delay) private {
     // TODO: Makes sure delay is short enough otherwise there is too much of a big advantage given to the user
-    
-    bets[userAddress] = Bet(betValue, false); // (uint betValue, bool putOption)
+    NewBetId += 1;
+    bets[NewBetId] = Bet(userAddress, betValue, false, 0) // (address userAddress, uint value, bool putOption // true = put : false = call, uint openPositionPrice)
     // TODO: make oraclize query with selected delay
+    betIdWithQueryId[??] = NewBetId;
   }
 
   // This can only be called from someone outside this contract
@@ -83,21 +95,72 @@ contract BinaryTrading is usingOraclize {
     }
   }
 
-  // This function is called when the get request results are ready
-  function __callback(bytes32 myid, string result, bytes proof) {
-    if (msg.sender != oraclize_cbAddress()) throw;
-    if (myidList[myid] == true) throw; // check if this myid was already processed before
-    myidList[myid] = true; // mark this myid (default bool value is false)
+   // ********************** //
+  // PUT POSITION FUNCTIONS //
+ // ********************** //
 
-    ETHUSD = parseInt(result, 2); // save it as $ cents
-    // do something with ETHUSD
-    updateExchangeRate(60); // Update data in 60 seconds
-  }
+ /*TODO:*/
 
-  // This function updates all the exchange rates
-  function updateExchangeRate(uint delay) {
-    // Costs $0.01 for URL call + $0.04 for TLSNotary = $0.05 per update, so it is important to only update when someone bets
-    oraclize_query(delay, "URL",
-      "json(https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD).USD");
-  }
+   // **************** //
+  // UPDATE FUNCTIONS //
+ // **************** //
+
+
+ function updateExchangeRateForCallRequest(uint delay, mapping bet) {
+   // Costs $0.01 for URL call + $0.04 for TLSNotary = $0.05 per update
+   oraclize_query(delay, "URL",
+     "json(https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD).USD");
+ }
+
+ // This function is called when the get request results are ready
+ function __callback(bytes32 myid, string result, bytes proof) {
+   if (msg.sender != oraclize_cbAddress()) throw;
+   if (myidList[myid] == true) throw; // check if this myid was already processed before
+   myidList[myid] = true; // mark this myid (default bool value is false)
+
+   ETHUSD = parseInt(result, 2); // save it as $ cents
+
+   // check openPositionPrice has been set or not and act upon that
+   if (bets[betIdWithQueryId[myid]].openPositionPrice == 0) {
+     bets[betIdWithQueryId[myid]].openPositionPrice == ETHUSD;
+   } else {
+     closePosition(betIdWithQueryId[myid])
+   }
+
+ }
+
+ // TODO: check that you can pass struct into a function
+ function closePosition(uint betId) private {
+   if (bets[betId].putOption) {
+     // bet was a put option: bet the value was going to go UP
+     if (bets[betId].openPositionPrice < ETHUSD) {
+       // Value went UP: wins bet
+       // TODO: Decide fair win %
+       userBalance[bets[betId].userAddress] += bet.value * (70/100) // reward +70% to the user
+     } else if (bets[betId].openPositionPrice > ETHUSD) {
+       // Value went DOWN: loses bet
+       userBalance[bets[betId].userAddress] -= bet.value
+     } else {
+       // Just take fee of executing transations
+       // TODO: Decide whether or not this is truely necessary
+       userBalance[bets[betId].userAddress] -= 0.10 * ETHUSD
+     }
+   } else {
+     // bet was a call option: bet the value was going to go DOWN
+     if (bets[betId].openPositionPrice > ETHUSD) {
+       // Value went DOWN: wins bet
+       // TODO: Decide fair win %
+       userBalance[bets[betId].userAddress] += bet.value * (70/100) // reward +70% to the user
+     } else if (bets[betId].openPositionPrice < ETHUSD) {
+       // Value went UP: loses bet
+       userBalance[bets[betId].userAddress] -= bet.value
+     } else {
+       // Just take fee of executing transations
+       userBalance[bets[betId].userAddress] -= 0.10 * ETHUSD
+     }
+   }
+ }
+
+
+
 }
