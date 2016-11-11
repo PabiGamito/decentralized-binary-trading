619c29cfccf5477fd8a11d335e6e4c18cf726313
Pablo Gamito <pablogamito@gmail.com>
2016-11-11 23:30:44 +0100

    fixed minor errors

--- main-contract.sol
+++ main-contract.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.3;
-import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+import "github.com/oraclize/ethereum-api/oraclizeAPI_0.4.sol";
 
 // TODO: Figure out how to calculate the max you can deposit so that the contract can't go bankrupt.
 // TODO: Make all function that can be private or whatever it is called so that no extrenal person can mess with them
@@ -30,11 +30,11 @@ contract BinaryTrading is usingOraclize {
   uint public ETHUSD;
 
   // Maps so that user balances are easily accessible by address
-  mapping (address => balance) userBalance;
+  mapping (address => uint) userBalance;
   uint brokerBalance;
   uint public returnRate;
 
-  struct Bet private {
+  struct Bet {
     address userAddress;
     uint value;
     bool putOption; // true = put : false = call
@@ -80,7 +80,7 @@ contract BinaryTrading is usingOraclize {
 
   // This function allows the user to withdraw his funds
   function withdraw() external returns (uint withdrawalBalance) {
-    withdrawalBalance = users[msg.sender].balance;
+    withdrawalBalance = userBalance[msg.sender];
     if (!msg.sender.send(withdrawalBalance)) throw;
   }
 
@@ -91,9 +91,9 @@ contract BinaryTrading is usingOraclize {
   }
 
   // User deposit
-  function deposit() payable external returns (uint balance) {
-    userBalance[msg.sender] += balance;
-    returns userBalance[msg.sender];
+  function deposit() payable external {
+    userBalance[msg.sender] += msg.value;
+
   }
 
   // *********************** //
@@ -111,7 +111,7 @@ contract BinaryTrading is usingOraclize {
     // Max delay is 30 minutes
     if (delay > 30*60) throw;
     latestBetId += 1;
-    bets[latestBetId] = Bet(userAddress, betValue, false, 0) // (address userAddress, uint value, bool putOption // true = put : false = call, uint openPositionPrice)
+    bets[latestBetId] = Bet(userAddress, betValue, false, 0); // (address userAddress, uint value, bool putOption // true = put : false = call, uint openPositionPrice)
     // TODO: make oraclize query with selected delay
     bytes32 myid = oraclize_query(0, "URL", "json(https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD).USD");
     betIdWithQueryId[myid] = latestBetId;
@@ -133,7 +133,7 @@ contract BinaryTrading is usingOraclize {
     // Max delay is 30 minutes
     if (delay > 30*60) throw;
     latestBetId += 1;
-    bets[latestBetId] = Bet(userAddress, betValue, true, 0) // (address userAddress, uint value, bool putOption // true = put : false = call, uint openPositionPrice)
+    bets[latestBetId] = Bet(userAddress, betValue, true, 0); // (address userAddress, uint value, bool putOption // true = put : false = call, uint openPositionPrice)
     // TODO: make oraclize query with selected delay
     bytes32 myid = oraclize_query(0, "URL", "json(https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD).USD");
     betIdWithQueryId[myid] = latestBetId;
@@ -158,7 +158,7 @@ contract BinaryTrading is usingOraclize {
      bytes32 newMyid = oraclize_query(delayWithQueryId[myid], "URL", "json(https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD).USD");
      betIdWithQueryId[newMyid] = betIdWithQueryId[myid];
    } else {
-     closePosition(betIdWithQueryId[myid])
+     closePosition(betIdWithQueryId[myid]);
    }
 
  }
@@ -169,30 +169,30 @@ contract BinaryTrading is usingOraclize {
      // bet was a put option: bet the value was going to go UP
      if (bets[betId].openPositionPrice < ETHUSD) {
        // Value went UP: WINS bet
-       brokerBalance -= bet.value * ReturnRate;
-       userBalance[bets[betId].userAddress] += bet.value * ReturnRate;
+       brokerBalance -= bets[betId].value * returnRate;
+       userBalance[bets[betId].userAddress] += bets[betId].value * returnRate;
      } else if (bets[betId].openPositionPrice > ETHUSD) {
        // Value went DOWN: LOSES bet
-       brokerBalance += bet.value;
-       userBalance[bets[betId].userAddress] -= bet.value;
+       brokerBalance += bets[betId].value;
+       userBalance[bets[betId].userAddress] -= bets[betId].value;
      } else {
        // Just take fee of executing transations
        // TODO: Decide whether or not this is truely necessary
-       userBalance[bets[betId].userAddress] -= 0.10 * ETHUSD;
+       // userBalance[bets[betId].userAddress] -= 0.10 * ETHUSD;
      }
    } else {
      // bet was a call option: bet the value was going to go DOWN
      if (bets[betId].openPositionPrice > ETHUSD) {
        // Value went DOWN: WINS bet
-       brokerBalance -= bet.value * ReturnRate;
-       userBalance[bets[betId].userAddress] += bet.value * returnRate;
+       brokerBalance -= bets[betId].value * returnRate;
+       userBalance[bets[betId].userAddress] += bets[betId].value * returnRate;
      } else if (bets[betId].openPositionPrice < ETHUSD) {
        // Value went UP: LOSES bet
-       brokerBalance += bet.value;
-       userBalance[bets[betId].userAddress] -= bet.value;
+       brokerBalance += bets[betId].value;
+       userBalance[bets[betId].userAddress] -= bets[betId].value;
      } else {
        // Just take fee of executing transations
-       userBalance[bets[betId].userAddress] -= 0.10 * ETHUSD;
+       // userBalance[bets[betId].userAddress] -= 0.10 * ETHUSD;
      }
    }
  }
