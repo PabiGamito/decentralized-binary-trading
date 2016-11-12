07c2a1930fa1ada0f90c9dbe5f761979829f0221
Pablo Gamito <pablogamito@gmail.com>
2016-11-12 23:37:57 +0100

    UI Updates

--- assets/sparkline.js
+++ assets/sparkline.js
@@ -11,8 +11,8 @@ var firstPricePointDate = null;
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas, false);
 function resizeCanvas() {
-  c.width = window.innerWidth;
-  c.height = window.innerHeight / 2;
+  c.width = $(".chart.card").width();
+  c.height = $(".chart.card").height();
 }
 
 var currentMousePos = { x: -1, y: -1 };
@@ -28,7 +28,7 @@ function updatePriceData(period, dataPoints) {
   updateBTC_ETHPrice(period, dataPoints);
 }
 
-updatePriceData(300, 50);
+updatePriceData(300, 100);
 
 // BTC TO USD //
 function updateCNY_BTCPrice(period, dataPoints) {
@@ -135,10 +135,17 @@ function renderCanvas() {
   var sparkLineTime = sparkLineEndTime - sparkLineBeginTime;
   var graphTime = secondsTillExpiration + sparkLineTime;
 
+  if ( $("#put").is(":hover") ) {
+    renderPutOverlay();
+  } else if ( $("#call").is(":hover") ) {
+    renderCallOverlay();
+  }
+
   sparkLineWidth = ( (c.width-margin*2-50) * sparkLineTime ) / graphTime;
 
   renderSparkLine(sparkLineWidth); // rendersSparkLine and returns SparkLine end Coordinates
   renderOptionExpirationLine(c.width - margin - 50);
+
   if ( $('canvas').is(":hover") ) {
     renderPointer(currentMousePos.x, currentMousePos.y);
   }
@@ -156,7 +163,7 @@ function priceToSparkData(priceData) {
   return spark;
 }
 
-function setGradiantColor(width = c.width) {
+function setGradiantColor() {
   // Define the line color gradiant
 	var grad = ctx.createLinearGradient(0, 0, c.width, c.height);
 	grad.addColorStop(0, "#007AC9");  // Initial path colour
@@ -166,48 +173,67 @@ function setGradiantColor(width = c.width) {
 }
 
 function renderSparkLine(width) {
-  renderPriceLabels(minVal(priceData)*1000, maxVal(priceData)*1000);
+  // renderPriceLabels(minVal(priceData)*1000, maxVal(priceData)*1000);
 
   var spark = priceToSparkData(priceData);
-  var ratioW = ( width - margin*2 ) / spark.length;
+  var ratioW = ( width - margin ) / spark.length;
 	var ratioH = ( c.height - margin*2 ) / maxVal(spark);
+  var lineWidth = 2;
 
 	var x = 0;
 	var y = 0;
 
   setGradiantColor(width);
 	ctx.beginPath();
-	ctx.lineWidth = "6";
+	ctx.lineWidth = lineWidth;
 	ctx.fill();
 	ctx.stroke();
 
 	for (var index in spark) {
-		if (index === 0) {
-			// First time
-			ctx.beginPath();
-			ctx.lineWidth = "6";
-			ctx.moveTo(margin, c.height - ( spark[index] * ratioH + margin ) );
-		} else {
-			x = index * ratioW + margin;
-			y = c.height - ( spark[index] * ratioH + margin );
-			ctx.lineTo(x,y);
-		}
+		x = index * ratioW;
+		y = c.height - ( spark[index] * ratioH + margin );
+		ctx.lineTo(x,y);
 	}
 
 	ctx.stroke();
 
 	ctx.beginPath();
-	ctx.lineWidth = "6";
+	ctx.lineWidth = lineWidth;
+
+  // Renders live price label
+  renderLivePriceLabel(y, parseFloat(priceData[priceData.length - 1])*1000);
 
   // Renders blinking dot
   if (frame % 2 === 0) {
     renderDot(x, y);
   }
+
+}
+
+function renderUnderSparkLineFadeColor(spark, ratioW, ratioH) {
+  ctx.beginPath();
+  ctx.lineWidth = 0;
+  for (var index in spark) {
+		x = index * ratioW;
+		y = c.height - ( spark[index] * ratioH + margin );
+		ctx.lineTo(x,y);
+	}
+  ctx.lineTo(x, c.height-margin);
+  ctx.lineTo(0, c.height-margin);
+  ctx.lineTo(0, c.height - ( spark[0] * ratioH + margin ));
+
+  ctx.closePath();
+	var grad = ctx.createLinearGradient(0, 0, 0, c.height);
+	grad.addColorStop(0, "rgba(0, 122, 201, 0.2)");  // Initial path colour
+	grad.addColorStop(1, "rgba(0, 201, 114, 0)");  // End stroke colour
+	// ctx.strokeStyle = grad;
+	ctx.fillStyle = grad;
+  ctx.fill();
 }
 
 function renderDot(x, y) {
   ctx.beginPath();
-  ctx.arc(x, y, 8, 0, 2 * Math.PI);
+  ctx.arc(x, y, 3, 0, 2 * Math.PI);
 	setGradiantColor();
   ctx.fill();
   ctx.stroke();
@@ -218,29 +244,30 @@ function renderPriceLabels(minPrice, maxPrice) {
   var priceInterval = (maxPrice-minPrice)/totalPriceLabels;
   var price = minPrice;
   for(i = 0; i<totalPriceLabels; i++) {
-    ctx.font = "bold 16px Arial";
+    ctx.font = "12px Arial";
+    ctx.fillStyle = "#747474";
     var y = c.height-margin-i*((c.height-(2*margin))/totalPriceLabels);
     ctx.fillText(price.toFixed(3), c.width-50, y);
-    renderHorizontalLine(y, "#E9E9E9", 1);
+    renderHorizontalLine(y, "#4e4e4e", 1);
     price += priceInterval;
   }
 }
 
 function renderHorizontalLine(y, color, width) {
+  ctx.lineWidth = width;
   ctx.beginPath();
   ctx.moveTo(0, y);
   ctx.lineTo(c.width, y);
   ctx.strokeStyle = color;
-  ctx.lineWidth = width;
   ctx.stroke();
 }
 
 function renderVerticalLine(x, color, width) {
+  ctx.lineWidth = width;
   ctx.beginPath();
   ctx.moveTo(x, 0);
-  ctx.lineTo(x, c.height);
+  ctx.lineTo(x, c.height-margin);
   ctx.strokeStyle = color;
-  ctx.lineWidth = width;
   ctx.stroke();
 }
 
@@ -251,8 +278,20 @@ function renderPointer(x, y) {
 }
 
 function renderPointerPrice(y, minPrice, maxPrice) {
-  ctx.font = "bold 16px Arial";
+  ctx.font = "12px Arial";
+  ctx.fillStyle = "#747474";
   price = ( (maxPrice-minPrice)*(c.height-y-margin) ) / (c.height-2*margin) + minPrice;
+  ctx.fillText(price.toFixed(3), c.width-50, y);
+}
+
+var livePriceY = null;
+function renderLivePriceLabel(y, price) {
+  livePriceY = y;
+  ctx.setLineDash([2, 2]);
+  renderHorizontalLine(y, "#8ea189", 2);
+  ctx.setLineDash([]);
+  ctx.font = "12px Arial";
+  ctx.fillStyle = "#747474";
   ctx.fillText(price.toFixed(3), c.width-50, y);
 }
 
@@ -261,10 +300,42 @@ function renderTimeLabels() {
 }
 
 function renderOptionExpirationLine(x) {
+  // var grad = ctx.createLinearGradient(0, 0, 0, c.height);
+	// grad.addColorStop(0, "#ffffff");  // Initial path colour
+	// grad.addColorStop(1, "#FB5229");  // End stroke colour
+  ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 0;
+  ctx.shadowBlur    = 7;
+  ctx.shadowColor   = "gray";
+  renderVerticalLine(x, "#FB5229", 2);
+  renderExpirationBubble(x);
+  ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 0;
+  ctx.shadowBlur    = 0;
+}
+
+function renderExpirationBubble(x) {
   ctx.beginPath();
-  ctx.moveTo(x, 0);
-  ctx.lineTo(x, c.height);
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = "3";
+  ctx.arc(x, c.height-margin, 8, 0, 2 * Math.PI);
+  ctx.fillStyle = "#FB5229";
+	ctx.strokeStyle = "#FB5229";
+  ctx.fill();
   ctx.stroke();
+}
+
+function renderPutOverlay() {
+	var grad = ctx.createLinearGradient(0, 0, 0, c.height);
+	grad.addColorStop(1, "rgba(255, 255, 255, 0)");  // Initial path colour
+	grad.addColorStop(0, "rgba(0, 207, 152, 0.8)");  // End stroke colour
+	// ctx.strokeStyle = grad;
+	ctx.fillStyle = grad;
+  ctx.fillRect(0, livePriceY, c.width, c.height);
+}
+function renderCallOverlay() {
+	var grad = ctx.createLinearGradient(0, 0, 0, c.height);
+	grad.addColorStop(1, "rgba(255, 50, 0, 0.8)");  // Initial path colour
+	grad.addColorStop(0, "rgba(0, 122, 201, 0)");  // End stroke colour
+	// ctx.strokeStyle = grad;
+	ctx.fillStyle = grad;
+  ctx.fillRect(0, 0, c.width, livePriceY);
 }
